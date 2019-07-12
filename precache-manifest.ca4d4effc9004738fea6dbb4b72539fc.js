4c8a02f70f052909aa715c03af11ca895453a8db
Hammad Balkhi <hammadbalkhi@Hammads-MacBook-Pro.local>
2019-07-11 23:58:39 -0400

    Updates

---
+++ precache-manifest.ca4d4effc9004738fea6dbb4b72539fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fea56839d4a26cb70ac",
+    "url": "/Lights-Out/static/css/main.27eff161.chunk.css"
+  },
+  {
+    "revision": "1fea56839d4a26cb70ac",
+    "url": "/Lights-Out/static/js/main.1fea5683.chunk.js"
+  },
+  {
+    "revision": "a6f3a2216de1c9165847",
+    "url": "/Lights-Out/static/js/1.a6f3a221.chunk.js"
+  },
+  {
+    "revision": "bcedacc98465ac2cb9bb",
+    "url": "/Lights-Out/static/js/runtime~main.bcedacc9.js"
+  },
+  {
+    "revision": "76e045086fd0b18c857dc165247b94d5",
+    "url": "/Lights-Out/index.html"
+  }
+];
